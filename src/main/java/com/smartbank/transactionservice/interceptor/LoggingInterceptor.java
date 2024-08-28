f5d78c1e61fee6fa495014867ccdf8d4c2e091ca
SachinBankar <SachinBankar1512@gmail.com>
2024-08-28 12:22:19 +0530

    Final Commit

--- src/main/java/com/smartbank/transactionservice/interceptor/LoggingInterceptor.java
+++ src/main/java/com/smartbank/transactionservice/interceptor/LoggingInterceptor.java
@@ -1,6 +1,8 @@
 package com.smartbank.transactionservice.interceptor;
 
+import org.apache.commons.text.StringEscapeUtils;
 import org.slf4j.MDC;
+import org.springframework.lang.Nullable;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerInterceptor;
 
@@ -12,10 +14,15 @@ import jakarta.servlet.http.HttpServletResponse;
 @Component
 public class LoggingInterceptor implements HandlerInterceptor{
 
-	 @Override
-	 public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
+	@Override
+	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
 				throws Exception {
-		 	MDC.put(SysConstant.SYS_REQ_CORR_ID_HEADER, request.getHeader(SysConstant.SYS_REQ_CORR_ID_HEADER));
+		 	MDC.put(SysConstant.SYS_REQ_CORR_ID_HEADER, StringEscapeUtils.escapeHtml4(request.getHeader(SysConstant.SYS_REQ_CORR_ID_HEADER)));
 			return true;
-		}
+	}
+	
+	@Override
+	public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler,@Nullable Exception ex) throws Exception {
+		MDC.remove(SysConstant.SYS_REQ_CORR_ID_HEADER);
+	} 
 }
